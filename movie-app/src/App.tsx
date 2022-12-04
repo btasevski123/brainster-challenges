97e830ea9984491041129daf37abb24bbd649a71
Boris Tasevski <btasevski123@gmail.com>
2022-12-04 20:57:53 +0100

    challenge- 19 solve

---
+++ movie-app/src/App.tsx
@@ -0,0 +1,26 @@
+
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Navbar } from './Components/Navbar/Navbar';
+import { ArtisItem } from './Components/ArtistItem/ArtistItem';
+import { ArtistPage } from './Components/ArtistPage/ArtistPage';
+import { AlbumPage } from './Components/AlbumPage/AlbumPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+    <Navbar />
+    <div className='Wrapper'>
+   <Routes>
+    <Route path='/' element={<ArtisItem />} />
+    <Route path='/artist/:id' element={<ArtistPage />} />
+    <Route path="/artist/:id/:albumId" element={<AlbumPage />} />
+   </Routes>
+    </div>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
